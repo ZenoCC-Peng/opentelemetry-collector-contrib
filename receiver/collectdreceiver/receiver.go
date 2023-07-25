ac887152c684ee5684624b24c7e676263626f130
ZenoCC-Peng <chengchuanp@splunk.com>
2023-07-24 22:13:52 -0700

    Change file to match with lint format

--- receiver/collectdreceiver/receiver.go
+++ receiver/collectdreceiver/receiver.go
@@ -13,11 +13,11 @@ import (
 	"strings"
 	"time"
 
-	"go.opentelemetry.io/collector/pdata/pmetric"
-
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/receiver"
+
 	"go.uber.org/zap"
 )
 
