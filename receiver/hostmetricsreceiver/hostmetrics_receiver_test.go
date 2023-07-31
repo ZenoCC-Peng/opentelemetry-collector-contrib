f80b7d4c9c80fb34333c4f720cedc2eba509c6d1
ZenoCC-Peng <chengchuanp@splunk.com>
2023-07-31 13:34:53 -0700

    change time format

--- receiver/hostmetricsreceiver/hostmetrics_receiver_test.go
+++ receiver/hostmetricsreceiver/hostmetrics_receiver_test.go
@@ -138,7 +138,7 @@ func TestGatherMetrics_EndToEnd(t *testing.T) {
 	cancelFn()
 
 	const tick = 50 * time.Millisecond
-	const waitFor = 10 * time.Second
+	const waitFor = 20 * time.Second
 	require.Eventuallyf(t, func() bool {
 		got := sink.AllMetrics()
 		if len(got) == 0 {
